c41004d508ff1fafa9b8f73be9440ccf61332f7b
sreeventh <sreeventhsrinivasan@gmail.com>
2024-02-21 11:16:50 +0530

    task2

--- first_react_app/src/App.jsx
+++ first_react_app/src/App.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import Header from "./home_components/Header"
 import Footer from "./home_components/Footer"
 import Note from "./home_components/Note"
+import notes_data from "./notes_data"
+
+function display_notes(a){
+  return(
+    <Note
+      title={a.title}
+      content={a.content}
+    />
+  )
+}
+
 function App() {
   return (
     <div>
       <Header/>
-      <Note/>
+      {notes_data.map(display_notes)}
       <Footer/>
     </div>
     
